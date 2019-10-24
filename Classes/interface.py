3e27bbe0f7ccc21f9cd038dbb71984aa6cd3b4ad
XaXallle4ka <33667573+XaXallle4ka@users.noreply.github.com>
2019-10-24 20:16:17 +0300

    Добавил баги с qt designer

---
+++ Classes/interface.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import *
+
+
+class MainWidgets(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('interface.ui', self)
+        # self.plankalkul_button.setGeometry(500, 0, 500, 150)
+        # self.Open_upgrade_menu()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainWidgets()
+    ex.show()
